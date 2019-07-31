bcddf0023c0c20eb82a436874c39a73275f21c59
JunShern <chanjunshern@gmail.com>
2019-07-31 21:07:54 +0800

    Add hide/show button

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
-let widthSlider, speedSlider, pauseButton;
+let widthSlider, speedSlider, pauseButton, hideButton;
 let currentX = 0;
 let playing = true;
+let hidden = false;
 
 function setup() {
   // put setup code here
@@ -11,12 +12,19 @@ function setup() {
   // create sliders
   widthSlider = createSlider(10, 500, 50, 10);
   widthSlider.position(20, 20);
+
   speedSlider = createSlider(-20, 20, 2, 2);
   speedSlider.position(20, 50);
+
   pauseButton = createButton('Pause');
   pauseButton.position(20, 80);
   pauseButton.mousePressed(togglePlayPause);
   pauseButton.size(speedSlider.width, 25);
+
+  hideButton = createButton('Hide');
+  hideButton.position(20, 110);
+  hideButton.mousePressed(toggleStripes);
+  hideButton.size(speedSlider.width, 25);
 }
 
 function togglePlayPause() {
@@ -25,6 +33,15 @@ function togglePlayPause() {
     pauseButton.html('Pause');
   } else {
     pauseButton.html('Play');
+  }
+}
+
+function toggleStripes() {
+  hidden = !hidden;
+  if (hidden) {
+    hideButton.html('Show');
+  } else {
+    hideButton.html('Hide');
   }
 }
 
@@ -37,8 +54,10 @@ function draw() {
   text('Speed: ' + speed, speedSlider.x * 2 + speedSlider.width, 65);
 
   if (playing) {
-  
     currentX = (currentX + speed) % (2 * lineWidth);
+  }
+  
+  if (!hidden) {
     let x = currentX - lineWidth;
     while (x < width) {
       rect(x, 0, lineWidth, height);
